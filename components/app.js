43c1b9ce0e4e3b090138544011bd158371ebbda6
EKMmattwindle <matthewwindle@ekm.com>
2022-11-11 08:23:17 +0000

    Added house component and images

--- components/app.js
+++ components/app.js
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import Banner from "./banner"
+import House from "./house";
 import HouseList from "./houseList"
 
 const App = () => {
+
+    const [selectedHouse, setSelectedHouse] = useState();
+
     return (
         <>
             <Banner headerText="Providing houses all over the world" />
-            <HouseList />
+            
+            {selectedHouse ? <House house={selectedHouse} /> : <HouseList />}
         </>
 
     )
